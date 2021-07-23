ee68598e92a0983f1962c06d4bbf822fad2078d3
Jorson007 <31435920+Jorson007@users.noreply.github.com>
2021-07-23 21:47:36 +0800

    Add files via upload
    实时图标代码提交

---
+++ NWRTChartDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NWRTChartDemo
+//
+//  Created by kwni on 2021/7/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
+
